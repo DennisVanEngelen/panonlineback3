7bbb13c5ba186fbcecedd0cb95376252c3651e0f
Dennis <Dennisvepc@gmail.com>
2021-05-26 15:37:55 +0200

    Initial setup

--- src/test/java/pandemiconline2/panonlinebackend/PanonlinebackendApplicationTests.java
+++ src/test/java/pandemiconline2/panonlinebackend/PanonlinebackendApplicationTests.java
@@ -1,15 +1,14 @@
 package pandemiconline2.panonlinebackend;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class PanonlinebackendApplicationTests
 {
 
     @Test
     void contextLoads()
     {
+
     }
 
 }
